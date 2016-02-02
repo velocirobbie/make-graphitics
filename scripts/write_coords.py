e400af3a654560a03a6ab988330522d83c9eeaab
Robert Sinclair <Robbie@RobMac.local>
2016-02-02 12:56:33 +0000

    reformatted lammps writer

--- scripts/write_coords.py
+++ scripts/write_coords.py
@@ -18,17 +18,33 @@ class Writer(object):
     def write_lammps(
             self,system_size,filename='data.lammps',system_name='comment line'):
         with open(filename,'w') as outfile:
-            outfile.write('# '+ system_name +'\n \n')
-            outfile.write(str(len(self.coords))+' atoms \n \n')
-            outfile.write('1 atom types \n \n')
-            outfile.write('0.0'+str(system_size[0])+' xlo xhi \n')
-            outfile.write('0.0'+str(system_size[1])+' ylo yhi \n')
-            outfile.write('0.0'+str(system_size[2])+' zlo zhi \n \n')
-            outfile.write('Pair Coeffs \n \n')
-            outfile.write('1   0.1 3.56    0.1     3.56 \n \n')
-            outfile.write('Atoms \n \n')
+            outfile.write(
+                    '# '+ system_name +
+                    str(len(self.coords)) +' atoms \n' 
+                    '\n'
+                    '1 atom types \n' 
+                    '\n'
+                    '0.0 \t'+str(system_size[0])+'\t xlo xhi \n'
+                    '0.0 \t'+str(system_size[1])+'\t ylo yhi \n'
+                    '0.0 \t'+str(system_size[2])+'\t zlo zhi \n'
+                    '\n'
+                    'Masses \n'
+                    '\n'
+                    '1 12.01 \n'
+                    '/n'
+                    'Pair Coeffs \n' 
+                    '\n'
+                    '1  0.1     3.56 \n' 
+                    '\n'
+                    'Atoms \n'
+                    '\n'
+                    )
+            
             i=1
             for atom in self.coords:
-                outfile.write(str(i)+' 1 1 '+str(atom[0])+str(atom[1])+str(atom[2])+' \n')
+                outfile.write(str(i)+'\t 1 1 0 '+str(atom[0])+'\t '+str(atom[1])+'\t '+str(atom[2])+' \n')
                 i+=1
+            
             print 'Coords written to '+str(filename)
+
+
