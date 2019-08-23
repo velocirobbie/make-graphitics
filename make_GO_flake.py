290c0e809ccf92da3f31daec2c90e9afb03a3249
Robert Sinclair <rcsinclair3@gmail.com>
2019-08-23 16:00:41 +0100

    update make_GO_flake

--- make_GO_flake.py
+++ make_GO_flake.py
@@ -6,12 +6,12 @@ import json
 
 config = yaml.load(open('config.yaml'))
 forcefield = 'OPLS'
-flake_radius = 50
+flake_radius = 250
 layout = [1,1,1] # make a 1x1x1 array of flakes
 
 motif = Hexagon_Graphene(config,forcefield,flake_radius)
 flake = Crystal(motif,config,forcefield,layout)
-Oxidiser(flake, ratio=2.5, video=20, new_island_freq=5e14, method='rf')
+Oxidiser(flake, ratio=2.5, video=20, new_island_freq=1e15, method='rf')
 name = 'GO_flake'
 output = Writer(flake,name)
 output.write_xyz(name+'.xyz')
