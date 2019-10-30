28f578b815c654536cdd7cd3e90f75cc106ae8cc
Robert Sinclair <robert.sinclair.15@ucl.ac.uk>
2019-10-30 15:15:49 +0000

    added rectangle build files

---
+++ build_files/make_GO_rect.py
@@ -0,0 +1,18 @@
+import yaml
+from scripts.molecules import *
+from scripts import *
+import numpy as np
+
+config = yaml.load(open('config.yaml'))
+forcefield = 'OPLS'
+
+motif = Rectangle_Graphene(config,forcefield,50,50)
+flake = Crystal(motif,config,forcefield,[1,1,1])
+Oxidiser(flake, ratio=2.5, video=20, new_island_freq=1e15, method='empirical')
+
+Parameterise(flake,vdw_defs)
+
+name = 'graphene'
+output = Writer(flake,name)
+output.write_xyz(name+'.xyz')
+output.write_lammps(name+'.data')
