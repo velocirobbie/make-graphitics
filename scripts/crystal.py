714c90bd34d48c9e0026c78e0db6375adbf92133
Robert Sinclair <robert.sinclair.15@ucl.ac.uk>
2019-11-25 11:50:01 +0000

    Crystal is now a child of Sim

--- scripts/crystal.py
+++ scripts/crystal.py
@@ -1,8 +1,9 @@
 from lattice import Lattice
 from connector import Connector
+from sim import Sim
 import numpy as np
 
-class Crystal(object):
+class Crystal(Sim):
     def __init__(self,molecule,config,
             forcefield=[],lattice_dimensions=[]):
 
@@ -51,39 +52,4 @@ class Crystal(object):
         connect = Connector()
         self.bonds = self.molecule.assign_bonds(
                 self.lattice_dimensions)
-
-    def generate_connections(self):
-        connect = Connector()
-        self.bond_types = connect.find_bond_types(self.atom_labels,self.bonds)
-        self.bond_labels = connect.bond_labels(
-                self.atom_labels,self.bonds,self.bond_types)
-       
-        self.bond_graph = self.generate_bond_graph(self.bonds)
-
-        self.angles = connect.angles(self.bonds, self.bond_graph)
-        self.angle_types = connect.find_angle_types(self.atom_labels,self.angles)
-        self.angle_labels = connect.angle_labels(
-                self.atom_labels,self.angles,self.angle_types)
-
-        self.dihedrals = connect.dihedrals(self.bonds, self.bond_graph)
-        self.dihedral_types = connect.find_dihedral_types(self.atom_labels,self.dihedrals)
-        self.dihedral_labels = connect.dihedral_labels(
-                self.atom_labels,self.dihedrals,self.dihedral_types)
-
-        self.impropers = connect.impropers(self.bonds, self.bond_graph)
-        self.improper_types = connect.find_improper_types(self.atom_labels,self.impropers)
-        self.improper_labels = connect.improper_labels(
-                self.atom_labels,self.impropers,self.improper_types)
-        
-    def generate_bond_graph(self, bonds):
-        N = int(np.amax(bonds)) # Number of atoms
-        bond_graph = dict()
-        for i in xrange(N):
-            bond_graph[i] = set()
-
-        for bond in bonds:
-            bond_graph[bond[0]-1].add(bond[1]-1)
-            bond_graph[bond[1]-1].add(bond[0]-1)
-        return bond_graph
-            
 
