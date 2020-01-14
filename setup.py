0c543e08e478174dd5614c6388c47a27dc3e7143
Robert Sinclair <robert.sinclair.15@ucl.ac.uk>
2020-01-14 18:11:38 +0000

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="makegraphitics",
+    version="0.2",
+    description="""Library to build graphene and graphite based structures
+                   for atomistic simulation""",
+    url="https://github.com/velocirobbie/make-graphitics",
+    author="Robert C Sinclair",
+    packages = find_packages(),
+)
